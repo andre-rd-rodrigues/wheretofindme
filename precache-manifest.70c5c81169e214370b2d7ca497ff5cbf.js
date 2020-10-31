08eb2a13c7bf68ca730dd8a771f65ed07b86d2dd
André Rodrigues <andrerodrigues@MacBook-Pro-de-Andre.local>
2020-10-31 16:05:01 +0000

    Updates

---
+++ precache-manifest.70c5c81169e214370b2d7ca497ff5cbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dc22a470e04895a67a69d689ccb8bce",
+    "url": "/where-to-find-me/index.html"
+  },
+  {
+    "revision": "18c626724fc68c414a9d",
+    "url": "/where-to-find-me/static/css/2.88c4c22b.chunk.css"
+  },
+  {
+    "revision": "c035cfe6eabb886d4f8e",
+    "url": "/where-to-find-me/static/css/main.1e4f734c.chunk.css"
+  },
+  {
+    "revision": "18c626724fc68c414a9d",
+    "url": "/where-to-find-me/static/js/2.3ba063c9.chunk.js"
+  },
+  {
+    "revision": "a7aec927f1e863a53de22e58e5329447",
+    "url": "/where-to-find-me/static/js/2.3ba063c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c035cfe6eabb886d4f8e",
+    "url": "/where-to-find-me/static/js/main.0bc935c4.chunk.js"
+  },
+  {
+    "revision": "1a2acbd98e21d7574716",
+    "url": "/where-to-find-me/static/js/runtime-main.3d6432ae.js"
+  }
+]);
